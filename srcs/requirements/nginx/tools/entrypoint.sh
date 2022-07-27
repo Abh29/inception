3698f30cc13c30ab7e22cd93dbe0697ffeb869a0
Abh29 <aboutifor@stud.kpfu.ru>
2022-07-28 01:43:21 +0300

    adding nginx config for nodejs

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -2,6 +2,7 @@
 
 ln -sf /etc/nginx/sites-available/mehill.42.fr /etc/nginx/sites-enabled
 ln -sf /etc/nginx/sites-available/adminer.42.fr /etc/nginx/sites-enabled
+ln -sf /etc/nginx/sites-available/mycv.42.fr /etc/nginx/sites-enabled
 
 if [ ! -e /etc/ssl/certs/ca.crt ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/ca.key -subj "/C=RU/ST=Tatarstan/L=Kazan/O=School21/CN=mehill/emailAddress=mehill@student.21-school.ru" -out /etc/ssl/certs/ca.crt
